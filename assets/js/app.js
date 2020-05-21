64fc1c909b7c512d11ec8e86cf882fd78d451ee6
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-21 07:54:39 -0500

    Added files

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+
+
+ var scrollRange = 100;
+
+ $(document).ready(function(){
+
+   // Initialize the Side Nav
+   $(".sidenav").sidenav();
+   $(".materialboxed").materialbox();
+   $(".parallax").parallax();
+   $(".tabs").tabs();
+   $(".datepicker").datepicker({
+     disableWeekends: true
+   });
+   $(".tooltipped").tooltip();
+   $(".scrollspy").scrollSpy();
+
+   $(window).scroll(function(){
+
+     if($(window).scrollTop() > scrollRange){
+       $("nav").addClass("white");
+       $(".brand-logo").addClass("black-text");
+       $("#nav_menu a").addClass("black-text");
+       $("nav").removeClass("transparent white-text");
+     }else{
+       $("nav").addClass("transparent white-text");
+       $(".brand-logo").removeClass("black-text");
+       $("#nav_menu a").removeClass("black-text");
+     }
+
+   });
+
+ });
