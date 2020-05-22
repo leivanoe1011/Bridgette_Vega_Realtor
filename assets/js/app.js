00ec8b9fc677f7cfb846b90cc2045bb78ebde0f1
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-21 21:21:25 -0500

    added transition properties to the CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,26 +16,20 @@
    $(".scrollspy").scrollSpy();
    $('.slider').slider();
 
-   $(window).scroll(function(){
+   
+   $(window).on("scroll",function(){
+    if ($(window).scrollTop()){
+      $("nav").addClass("scroll_transition black-text");
+      $(".brand-logo").addClass("black-text");
+      $("nav").removeClass("transparent white-text");
+      $("#nav_menu a").addClass("black-text");
+    }
+    else{
+      $("nav").removeClass("scroll_transition black-text");
+      $(".brand-logo").removeClass("black-text");
+      $("#nav_menu a").removeClass("black-text");
+    }
+   })
 
-     if($(window).scrollTop() > scrollRange){
-       $("nav").addClass("white");
-       $("nav").addClass("nav_expand");
-      //  $("nav").addClass("scale-transition scale-in");
-       $(".brand-logo").addClass("black-text");
-       $("#nav_menu a").addClass("black-text");
-       $("nav").removeClass("transparent white-text");
-
-       $("#menu_icon").addClass("black-text");
-     }else{
-       $("nav").addClass("transparent white-text");
-       $("nav").removeClass("nav_expand");
-       $(".brand-logo").removeClass("black-text");
-       $("#nav_menu a").removeClass("black-text");
-
-       $("#menu_icon").removeClass("black-text");
-     }
-
-   });
 
  });
