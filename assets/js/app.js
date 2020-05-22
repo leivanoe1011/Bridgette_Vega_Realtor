56a1d3b18f14b3f38ef14c293e34ff838598c0e8
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-21 21:41:37 -0500

    loaded the sidenav method in app js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
 
 
- var scrollRange = 100;
 
  $(document).ready(function(){
 
    // Initialize the Side Nav
-   $(".sidenav").sidenav();
+   $('.sidenav').sidenav();
    $(".materialboxed").materialbox();
    $(".parallax").parallax();
    $(".tabs").tabs();
@@ -16,18 +15,20 @@
    $(".scrollspy").scrollSpy();
    $('.slider').slider();
 
-   
+
    $(window).on("scroll",function(){
     if ($(window).scrollTop()){
       $("nav").addClass("scroll_transition black-text");
       $(".brand-logo").addClass("black-text");
       $("nav").removeClass("transparent white-text");
       $("#nav_menu a").addClass("black-text");
+      $("#menu_icon").addClass("black-text");
     }
     else{
       $("nav").removeClass("scroll_transition black-text");
       $(".brand-logo").removeClass("black-text");
       $("#nav_menu a").removeClass("black-text");
+      $("#menu_icon").removeClass("black-text");
     }
    })
 
